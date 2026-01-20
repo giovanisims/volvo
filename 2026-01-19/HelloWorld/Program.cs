fcd5d4b1ce75a1c27aed65616799925b91878947
giovanisims <giovani@simoes.pro>
2026-01-19 22:22:58 -0300

    2026-01-19

---
+++ 2026-01-19/HelloWorld/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace HelloWorld;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Type your name");
+        string name = Console.ReadLine();
+        
+        Console.WriteLine($"Hello {name}!");
+
+        Console.WriteLine("Please insert your CPF");
+        string CPF = Console.ReadLine();
+        Console.WriteLine($"You inserted {CPF}");
+
+        Console.WriteLine("Please insert your cellphone");
+        string cellphone = Console.ReadLine();
+        Console.WriteLine($"You inserted {cellphone}");
+
+        Console.WriteLine("Please insert your birth year");
+        int birthyear = int.Parse(Console.ReadLine());
+        Console.WriteLine($"You inserted {birthyear}");
+
+    }
+}
